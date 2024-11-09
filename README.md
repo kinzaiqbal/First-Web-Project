474bbeea41360e31c1eec0b303455b8a450006e9
kinza iqbal <kinzaiqbal412@gmail.com>
2024-11-10 00:14:41 +0500

    first commit

--- README.md
+++ README.md
@@ -41,3 +41,6 @@ Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/bui
 "# First-Web-Project" 
 "# First-Web-Project" 
 "# First-Web-Project" 
+"# First-Web-Project" 
+"# First-Web-Project" 
+"# First-Web-Project" 
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -29,7 +29,7 @@ export default function Contact() {
             {/* Message */}
             <textarea
               placeholder="Your Message"
-              rows ="4"
+              rows ={4}
               className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             ></textarea>
 
